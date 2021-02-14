47b0b302d51d1caad1e367f4ec656761ca550c53
Phoenix Work <Work@Phoenixs-MacBook-Pro.local>
2021-02-14 03:07:50 -0800

    done

--- 01/app.js
+++ 01/app.js
@@ -5,16 +5,24 @@ const loadList = JSON.parse(localStorage.getItem("tasks")) || [];
 for (let i = 0; i < loadList.length; i++)
 {
     let newTask = document.createElement('li')
+    let newBtn = document.createElement('button')
+    newBtn.style.padding = '5px'
     newTask.innerText = loadList[i].task
     newTask.isDone = loadList[i].isDone ? true : false;
     if (newTask.isDone)
     {
         newTask.style.textDecoration = "line-through"
     }
+    newTask.append(newBtn)
     theList.append(newTask)
 }
 
 theList.addEventListener("click", function(event) {
+    if (event.target.tagName === "BUTTON")
+    {
+        event.target.parentElement.remove()
+        localStorage.removeItem("tasks", JSON.stringify(loadList))
+    }
     if (!event.target.isDone)
     {
        event.target.style.textDecoration = "line-through"
@@ -38,11 +46,14 @@ theList.addEventListener("click", function(event) {
 formElm.addEventListener("submit", function(event) {
     event.preventDefault();
     let newTask = document.createElement('li')
+    let newBtn = document.createElement('button')
     let newText = document.querySelector('#Todo-task')
 
+    newBtn.style.padding = '5px'
     newTask.innerText = newText.value
     newTask.isDone = false;
     theList.append(newTask)
+    newTask.append(newBtn)
     formElm.reset();
 
     loadList.push({task: newTask.innerText, isDone: false})
